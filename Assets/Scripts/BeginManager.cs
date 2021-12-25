b935a1de9539e880df0edd81ce036912539cbdd9
Daniil Novoselov <gudnmail@gmail.com>
2021-12-25 13:10:03 +0500

    feat(begin): add requesting permission

---
+++ Assets/Scripts/BeginManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BeginManager : MonoBehaviour
+{
+  private const string prefsKey = "permissionsGranted";
+  //   private string[] permissions = { Permission.Camera, Permission.Microphone };
+  private string[] permissions = { };
+  [SerializeField]
+  private Button nextButton;
+
+
+  private int grantedCount = 0;
+
+  internal void Denied(string permission)
+  {
+    print(permission);
+    PlayerPrefs.SetInt(prefsKey, 0);
+    PlayerPrefs.Save();
+    Application.Quit();
+  }
+
+  internal void Granted(string permission)
+  {
+    grantedCount++;
+    if (grantedCount >= permissions.Length)
+    {
+      PlayerPrefs.SetInt(prefsKey, 1);
+      PlayerPrefs.Save();
+      SceneManager.LoadScene("Main");
+    }
+  }
+
+  void RequestPermissons()
+  {
+    if (permissions.Length == 0)
+    {
+      PlayerPrefs.SetInt(prefsKey, 1);
+      PlayerPrefs.Save();
+      SceneManager.LoadScene("Main");
+      return;
+    }
+    var callbacks = new PermissionCallbacks();
+    callbacks.PermissionDenied += Denied;
+    callbacks.PermissionDeniedAndDontAskAgain += Denied;
+    callbacks.PermissionGranted += Granted;
+    Permission.RequestUserPermissions(permissions, callbacks);
+  }
+
+  void Start()
+  {
+    nextButton.onClick.AddListener(RequestPermissons);
+    if (PlayerPrefs.GetInt(prefsKey, 0) == 1)
+    {
+      RequestPermissons();
+    }
+  }
+}
